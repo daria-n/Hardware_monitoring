44e3be4119334312c683960e8bdaa2dcd1c73045
Mateusz Tasz <mateusz.tasz@gmail.com>
2016-09-23 22:35:56 +0200

    Add GET request in ajax and new charts.

--- hwdata/views.py
+++ hwdata/views.py
@@ -2,19 +2,31 @@ from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
 from .models import Data
 from psutil import cpu_percent, swap_memory
+from django.core import serializers
 
 
 # Create your views here.
 def index(request):
     count = Data.objects.count()
     my_data = Data.objects.all()[count-1:]
-    return render(request, 'hwdata/index.html', {'current_data': my_data})
+    return render(request, 'hwdata/index2.html', {'current_data': my_data})
 
 
 def json(request):
-    cpu_current_usage = cpu_percent()
-    swap_current_usage = (100 * swap_memory().used) / swap_memory().total
-    swap_current_usage = round(swap_current_usage, 2)
-    Data(CPU_usage=cpu_current_usage, SWAP_usage=swap_current_usage).save()
-    context = {'cpu usage': cpu_current_usage, 'swap usage': swap_current_usage}
-    return JsonResponse(context)
+
+    if request.is_ajax():
+        cpu_current_usage = cpu_percent()
+        swap_current_usage = (100 * swap_memory().used) / swap_memory().total
+        swap_current_usage = round(swap_current_usage, 2)
+        Data(CPU_usage=cpu_current_usage, SWAP_usage=swap_current_usage).save()
+        context = {'cpu usage': cpu_current_usage, 'swap usage': swap_current_usage}
+        return JsonResponse(context)
+
+    else:
+        my_data = Data.objects.all()
+        return render(request, 'hwdata/json.html', {'current_data': my_data})
+
+
+
+
+
