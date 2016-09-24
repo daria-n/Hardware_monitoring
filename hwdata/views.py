0e0b5b65ea259c80adb072ba2c825507fd858e1b
tandaradei <nowickadaria1@gmail.com>
2016-09-24 10:09:17 +0100

    Doesn't work perfectly, but keeps only 500 latest records in the database

--- hwdata/views.py
+++ hwdata/views.py
@@ -19,14 +19,11 @@ def json(request):
         swap_current_usage = (100 * swap_memory().used) / swap_memory().total
         swap_current_usage = round(swap_current_usage, 2)
         Data(CPU_usage=cpu_current_usage, SWAP_usage=swap_current_usage).save()
+        id_list = Data.objects.all()[:500].values_list("id", flat=True)  # only retrieve ids.
+        Data.objects.exclude(pk__in=list(id_list)).delete()
         context = {'cpu usage': cpu_current_usage, 'swap usage': swap_current_usage}
         return JsonResponse(context)
 
     else:
         my_data = Data.objects.all()
         return render(request, 'hwdata/json.html', {'current_data': my_data})
-
-
-
-
-
